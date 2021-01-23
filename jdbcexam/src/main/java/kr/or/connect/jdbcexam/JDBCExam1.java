c06211bb5672756654f8762c5ed55a82b9600cf4
root <tldms9700@gmail.com>
2021-01-23 22:36:05 +0900

    SpringJDBC실습

--- jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam1.java
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam1.java
@@ -1,13 +1,23 @@
 package kr.or.connect.jdbcexam;
 
+import java.util.List;
+
 import kr.or.connect.jdbcexam.dao.RoleDao;
 import kr.or.connect.jdbcexam.dto.Role;
 
 public class JDBCExam1 {
 	public static void main(String[] args) {
+		// 한개 select
 		RoleDao dao  = new RoleDao();
 		Role role = dao.getRole(100);
 		System.out.println(role);
 		
+		//여러개 select
+		RoleDao daos = new RoleDao();
+		List<Role> list = daos.getRoles();
+		
+		for(Role roles : list) {
+			System.out.println(roles);
+		}
 	}
 }
--- jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.or.connect.jdbcexam.dto.Role;
 
@@ -12,6 +14,35 @@ public class RoleDao {
 	private static String dburl = "jdbc:mysql://localhost:3306/spring";
 	private static String dbUser = "root";
 	private static String dbpasswd = "mm1125";
+	
+	public List<Role> getRoles(){
+		List<Role> list = new ArrayList<>();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException e ) {
+			e.printStackTrace();
+		}
+		
+		String sql = "SELECT role_id, description FROM role order by role_id desc";
+		try(Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)){
+			
+			try(ResultSet rs = ps.executeQuery()){
+				while(rs.next()) {
+					int id = rs.getInt("role_id");
+					String description = rs.getString(2);
+					Role role = new Role(id, description);
+					list.add(role);
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return list;
+	}
 	
 	public int addRole(Role role) {
 		int insertCount = 0;
